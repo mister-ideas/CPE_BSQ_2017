57bc129a0d3abf38cf795dc445a6a63c95e7645f
Vincent Attali <vincent.attali@epitech.eu>
2017-12-11 17:08:15 +0100

    "added file buffer"

---
+++ include/bsq.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2017
+** BSQ
+** File description:
+** bsq.h
+*/
+
+#ifndef BSQ_H_
+#define BSQ_H_
+
+
+
+#endif
--- src/bsq.c
+++ src/bsq.c
@@ -5,7 +5,33 @@
 ** bsq.c
 */
 
-int main(void)
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "my.h"
+#include "bsq.h"
+
+int main(int ac, char **av)
 {
+	int fd = open(av[1], O_RDONLY);
+	char *buff;
+	struct stat s;
+	int i = 0;
+
+	if (ac != 2)
+		return (84);
+	else if (fd == -1)
+		return (84);
+	stat(av[1], &s);
+	if ((buff = malloc(s.st_size + 1)) == NULL)
+		return (84);
+	while (read(fd, buff + i, 4096) > 0)
+	       i += 4096;
+	close(fd);
+	buff[s.st_size] = '\0';
+	my_putstr(buff);
+	free(buff);
 	return (0);
 }
